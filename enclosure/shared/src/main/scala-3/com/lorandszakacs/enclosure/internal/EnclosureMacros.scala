25e5accdb300de35c459920b514d615554130d4e
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-04-04 08:57:04 +0300

    Implement Scala 3 macros

--- enclosure/shared/src/main/scala-3/com/lorandszakacs/enclosure/internal/EnclosureMacros.scala
+++ enclosure/shared/src/main/scala-3/com/lorandszakacs/enclosure/internal/EnclosureMacros.scala
@@ -18,6 +18,7 @@ package com.lorandszakacs.enclosure.internal
 
 import com.lorandszakacs.enclosure.Enclosure
 
+import scala.annotation.tailrec
 import scala.quoted.*
 
 trait EnclosureMacros {
@@ -25,10 +26,60 @@ trait EnclosureMacros {
 }
 
 object EnclosureMacros {
-    def enclosure_impl(using ctx: Quotes): Expr[Enclosure] = {
-    val rootPosition = ctx.reflect.Position.ofMacroExpansion
-    val file = Expr(rootPosition.sourceFile.jpath.toString)
-     //WIP: definitely wrong
-    '{Enclosure($file)}
+
+  def enclosure_impl(using ctx: Quotes): Expr[Enclosure] = {
+    val helpers  = new EnclosingModuleHelpers(using ctx)
+    val moduleName = Expr(helpers.getFullyQualifiedNameOfEnclosingModule)
+    '{Enclosure($moduleName)}
+  }
+
+  private final class EnclosingModuleHelpers(using ctx: Quotes) {
+    import ctx.reflect.*
+    /** Where "module" is a:
+      * - class
+      * - object
+      * - package object
+      */
+    def getFullyQualifiedNameOfEnclosingModule: String = {
+      val moduleSym: Symbol = findEnclosingModule(enclosingOwner)
+      fullNameFromSymbol(moduleSym)
+    }
+
+    @tailrec private def findEnclosingModule(sym: Symbol): Symbol = {
+      sym match {
+        case s if s.isNoSymbol                     =>
+          report.throwError("Couldn't find an enclosing class or module for the logger")
+        case s if s.isClassDef => s
+        case other                        =>
+          //TODO: gather all methods along the way so that we can fill in that information as well
+          findEnclosingModule(other.owner)
+      }
+    }
+
+    private def enclosingOwner: Symbol = Symbol.spliceOwner
+
+    @tailrec private def fullNameFromSymbol(sym: Symbol): String = {
+      val flags = sym.flags
+      if (flags.is(Flags.Package)) {
+        sym.fullName
+      }
+      else if (sym.isClassDef) {
+        if (flags.is(Flags.Module)) {
+          if (sym.name == "package$") {
+            fullNameFromSymbol(s.owner)
+          }
+          else {
+            val chomped = sym.name.stripSuffix("$")
+            fullNameFromSymbol(sym.owner) + "." + chomped
+          }
+        }
+        else {
+          fullNameFromSymbol(sym.owner) + "." + sym.name
+        }
+      }
+      else {
+        fullNameFromSymbol(sym.owner)
+      }
+    }
   }
 }
